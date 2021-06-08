f4dcbd83ef9cb6e2ec0a890f511afd58c166abec
Beatriz Milz <beatriz.milz@hotmail.com>
2021-06-08 19:43:13 -0300

    remove arquivo desnecessário

--- R/3-importar-texto.R
+++
@@ -1,5 +0,0 @@
-fs::dir_ls("data-raw/2-texto-arquivos/") %>% length()
-
-arquivos_texto <- lista_de_arquivos %>%
-  dplyr::mutate(caminho_ = glue::glue("data-raw/1-arquivos-pdf/{nome_limpo}"),
-                texto = pdftools::pdf_text(pdf = ))
