1d15d16292c4e6040d3ecabbdd678ddaaac4e37d
Beatriz Milz <beatriz.milz@hotmail.com>
2021-06-08 20:19:28 -0300

    melhora busca

--- data-raw/4-buscar-temas.R
+++ data-raw/4-buscar-temas.R
@@ -2,11 +2,13 @@ termos_pesquisar <- c("servicos ecossistemicos|ecosystem services")
 
 df_limpo <- df_arquivos %>%
   dplyr::select(-texto) %>%
-  dplyr::mutate(texto_limpo = abjutils::rm_accent(texto_limpo))
+  dplyr::mutate(texto_limpo = abjutils::rm_accent(texto_limpo),
+                texto_limpo = stringr::str_to_lower(texto_limpo))
 
 
 df_limpo %>%
   dplyr::mutate(detectado = stringr::str_detect(texto_limpo, termos_pesquisar)) %>%
-  dplyr::filter(detectado == TRUE) %>% View()
-  dplyr::pull(texto_limpo) %>%
-  print()
+  dplyr::filter(detectado == TRUE) %>%
+  dplyr::count(nome_arquivo)
+  # dplyr::pull(texto_limpo) %>%
+  # print()
